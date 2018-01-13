2aa2eb905903da5a4dfe25c2c98c03746ee1ed0f
Andrii Nadezhdin <triviy@gmail.com>
2018-01-13 21:18:35 +0200

    Fix raiting

--- Core/ImageManagers/ContentProviders/NevsedomaBaseProvider.cs
+++ Core/ImageManagers/ContentProviders/NevsedomaBaseProvider.cs
@@ -8,7 +8,7 @@
         protected abstract string RaitingPattern { get; }
         protected abstract string Category { get; }
 
-        public string PostPattern => $"<a.*?class=\"s-img\".*?href=\"(.*?{_rootUri}/index.php[?]newsid=.*?)\">[\\s\\S]*?{RaitingPattern}[\\s\\S]*?</a>";
+        public string PostPattern => $"<a.*?class=\"s-img\".*?href=\"(.*?{_rootUri}/index.php[?]newsid=.*?)\">[\\s\\S]{{1,900}}{RaitingPattern}";
         public virtual string ImagePattern => $"<img.*?src=\"(.*?{_rootUri}.*?jpg)\".*?/>";
 
         public string GenerateIndexPageUrl(int page)
--- Core/ImageManagers/ContentProviders/NevsedomaVideoProvider.cs
+++ Core/ImageManagers/ContentProviders/NevsedomaVideoProvider.cs
@@ -2,8 +2,8 @@
 {
     internal class NevsedomaVideoProvider : NevsedomaBaseProvider
     {
-        protected override string RaitingPattern => "<span.*?rating.*?>\\+([2-9][0-9]|[1-9][0-9][0-9]).*?</span>";
+        protected override string RaitingPattern => "<span.*?rating.*?>\\+([3-9][0-9]|[1-9][0-9][0-9]).*?</span>";
         protected override string Category => "vzrosloe_jerotika";
-        public override string ImagePattern => $"\"(.*?{_rootUri}.*?mp4)\"";
+        public override string ImagePattern => $"<source.*?src=\"(.*?{_rootUri}.*?(webm|mp4))\".*?>";
     }
 }
