4efb199c4d0166cff06f4dda39da8a7d4eae0af2
Andrii Nadezhdin <triviy@gmail.com>
2020-03-27 23:24:55 +0200

    Fix after fix

--- Core/ImageManagers/ContentProviders/KorzikEromiksProvider.cs
+++ Core/ImageManagers/ContentProviders/KorzikEromiksProvider.cs
@@ -7,7 +7,7 @@
 
         public string PostPattern => $"<a .*?href=\"(.*?{_rootUri}/erotika/.*?(eromiks|ero-miks).*?.html)\".*?>";
         public string ImagePattern => $"<img.*?src=\"(.*?uploads/posts.*?.jpg)\".*?/>";
-        public string RootUri => $"{_schema}//{_rootUri}";
+        public string RootUri => $"{_schema}://{_rootUri}";
 
         public string GenerateIndexPageUrl(int page)
         {
